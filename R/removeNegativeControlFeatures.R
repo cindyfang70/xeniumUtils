49fc970ca6dcb5341dcbe698fef16b8d000bbbdf
Cindy Fang <cindyfang70@gmail.com>
2024-04-14 20:00:24 -0400

    add a function for removing negative control probes

---
+++ R/removeNegativeControlFeatures.R
@@ -0,0 +1,9 @@
+#' Remove negative control probe and other technical features from Xenium In Situ data
+#'
+#' @param x a SpatialFeatureExperiment or SpatialExperiment object
+#'
+#' @return x, but only containing gene features
+#' @export
+removeNegativeControlFeatures <- function(x){
+  x <- x[which(rowData(sfe)$Type=="Gene Expression"), ]
+}
